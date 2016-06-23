08eadc1e9397458ae3dc56dc69ed67b36f1bbf9b
skychb <gm@haeyum.org>
2016-06-23 21:34:40 +0900

    first step (#103)

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,1 +1,14 @@
-var a;
+var todo = document.getElementById("new-todo");
+var template = '<li class={{#if completed}}"completed"{{/if}}><div class="view"><input class="toggle" type="checkbox"{{#if completed}}checked{{/if}}><label>{{inputValue}}</label><button class="destroy"></button></div><input class="edit" value={{inputValue}}></li>'
+var template_up = Handlebars.compile(template);
+
+if(todo){
+  todo.addEventListener('keypress', function (e) {
+      var key = e.which || e.keyCode;
+      if (key === 13) {
+        var list = document.getElementById("todo-list");
+        list.insertAdjacentHTML('beforeend', template_up({"inputValue":todo.value}));
+        todo.value = "";
+       }
+  });
+}
