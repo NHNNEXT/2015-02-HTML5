011948773c05ad231e51d559457035cefaa528ba
Junnie-Jobs <dj0999@naver.com>
2016-07-08 12:32:17 +0900

    3week assignment

--- asset/nativeJS.js
+++ asset/nativeJS.js
@@ -8,37 +8,51 @@ var newTodoList =
     "</div>";
 
 function addTODO(e){
-
   if(e.keyCode === ENTER_KEY){
+
       var content = document.getElementById("new-todo").value;
-      var newTodo = document.createElement("li");
-      newTodo.innerHTML = newTodoList.replace(/\{\{input-value\}\}/gi, content);
-      document.getElementById("todo-list").appendChild(newTodo);
-      newTodo.className = "appending";
-      setTimeout(function () {
+      TODOSync.add(content, function(json){
+          var newTodo = document.createElement("li");
+          newTodo.className = "appending";
+          newTodo.dataset.id = json.insertId;
+          newTodo.innerHTML = newTodoList.replace(/\{\{input-value\}\}/gi, content);
+          document.getElementById("todo-list").appendChild(newTodo);
+          newTodo.offsetHeight;
           newTodo.className = "";
-      }, 10);
-      document.getElementById("new-todo").value = "";
+          document.getElementById("new-todo").value = "";
+      })
     }
  }
+   //  newTodo.addEventListener("webkitTransitionEnd", function(){ newTodo.className = ""});
 
 function completedTODO(e){
     var input = e.target;
     var li = input.parentNode.parentNode;
-    if(input.checked){
-      li.className = "completed";
-    }else{
-      li.className = "";
-    }
+    var completed = input.checked?"1":"0";
+
+    TODOSync.completed({
+      "id" : li.dataset.id,
+      "completed" : completed
+    }, function(){
+        if(completed === "1"){
+            li.className = "completed";
+        }else{
+            li.className = "";
+        }
+    })
 }
 
 function removeTODO(e){
   var li = e.target.parentNode.parentNode;
   li.className= "deleting";
-  setTimeout(function () {
-      li.remove;
-  }, 100);
-}
+  TODOSync.remove({
+      "id" : li.dataset.id
+      }, function(){
+        li.addEventListener("transitionend",function(){
+             li.remove();
+      });
+    })
+  }
 
 function todoEventCheck(e){
   var target = e.target;
@@ -49,10 +63,24 @@ function todoEventCheck(e){
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(){
+function loadTodo(){
+  TODOSync.get(function(data){
+    var newTodo = document.createElement("li");
+    for(var i = data.length-1; i >= 0 ; i--){
+      var newTodo = document.createElement("li");
+      newTodo.dataset.id = data[i].id;
+      newTodo.innerHTML = newTodoList.replace(/\{\{input-value\}\}/gi, data[i].todo);
+      document.getElementById("todo-list").appendChild(newTodo);
+      if(data[i].completed === 1){
+        newTodo.className = "completed";
+        newTodo.firstChild.firstChild.checked = true;
+      }
+    }
+  });
+}
 
+document.addEventListener("DOMContentLoaded", function(){
+    loadTodo();
     document.getElementById("new-todo").addEventListener("keydown", addTODO);
     document.getElementById("todo-list").addEventListener("click", todoEventCheck);
-
-
 });
