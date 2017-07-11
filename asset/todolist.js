59b4f5652e169e9545a129f778314f94761163a1
bbq923 <bbq9234@naver.com>
2017-07-11 10:23:03 +0900

    basic complete & delete

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,8 +1,8 @@
 function addTODO(e) {
     var ENTER_KEYCODE = 13;    
-    if(e.keyCode === ENTER_KEYCODE) {
+    if(e.keyCode === ENTER_KEYCODE && this.value !== "") {
         var todoList = document.getElementById("todo-list");
-        var todo = document.getElementById("new-todo").value;
+        var todo = this.value;
         var todos = [{
             "value": todo
         }];
@@ -12,11 +12,29 @@ function addTODO(e) {
 
         //string 값인 todoElement를 li DOM node 로 만들어준다
         var el = document.createElement("li");
-        el.innerHTML = todoElement;
+        console.log(todoElement);
+        var start = todoElement.indexOf("<div");
+        var end = todoElement.lastIndexOf("</li>");
+
+        el.innerHTML = todoElement.substring(start, end);
         todoList.insertBefore(el, todoList.firstChild);
 
-        document.getElementById("new-todo").value = "";
+        this.value = "";
+    }
+}
+
+function completeTODO(e) {
+    if(e.target.classList.contains("toggle")) {
+        e.target.parentNode.parentNode.classList.toggle("completed");
+    }
+}
+
+function deleteTODO(e) {
+    if(e.target.classList.contains("destroy")) {
+        e.currentTarget.removeChild(e.target.parentNode.parentNode);
     }
 }
 
 document.getElementById("new-todo").addEventListener("keydown", addTODO);
+document.getElementById("todo-list").addEventListener("click", completeTODO);
+document.getElementById("todo-list").addEventListener("click", deleteTODO);
