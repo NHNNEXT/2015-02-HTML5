1ce8fa5c1445df135dddf0556d3c8242ae46ec5a
bbq923 <bbq9234@naver.com>
2017-07-17 19:17:53 +0900

    use keyframe animation for loading & delete

--- asset/todolist.js
+++ asset/todolist.js
@@ -24,10 +24,20 @@ function completeTODO(e) {
 
 function deleteTODO(e) {
     if(e.target.classList.contains("destroy")) {
-        e.currentTarget.removeChild(e.target.parentNode.parentNode);
+        e.target.parentNode.parentNode.classList = "delete";
+        // e.currentTarget.removeChild(e.target.parentNode.parentNode);
+    }
+}
+
+function removeTODO(e) {
+    if(e.animationName === "slideout" && e.target.classList.contains("delete")) {
+        // e.target.parentNode.parentNode.classList = "delete";
+        
+        e.currentTarget.removeChild(e.target);
     }
 }
 
 document.getElementById("new-todo").addEventListener("keydown", addTODO);
 document.getElementById("todo-list").addEventListener("click", completeTODO);
 document.getElementById("todo-list").addEventListener("click", deleteTODO);
+document.getElementById("todo-list").addEventListener("animationend", removeTODO);
