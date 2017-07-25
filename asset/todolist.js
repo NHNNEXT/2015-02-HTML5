7cc390f11cdd140e2f4ca96992b8cb0a4e42c890
bbq923 <bbq9234@naver.com>
2017-07-25 11:33:10 +0900

    use fetch to cors

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,43 +1,89 @@
-function addTODO(e) {
-    var ENTER_KEYCODE = 13;    
-    if(e.keyCode === ENTER_KEYCODE && this.value !== "") {
-        var todoList = document.getElementById("todo-list");
-        var todo = this.value;
-        var todos = [{
-            "value": todo
-        }];
-        var todoElement = template({
-            "todos": todos
-        });
+var TODO = {
+    ENTER_KEYCODE : 13,
+    URL : "http://128.199.76.9:8002/bbq923/todo",
 
-        todoList.insertAdjacentHTML("afterbegin", todoElement);
+    init : function() {
+        document.addEventListener("DOMContentLoaded", function() {
+            document.getElementById("new-todo").addEventListener("keydown", this.add.bind(this));
+            document.getElementById("todo-list").addEventListener("click", this.complete.bind(this));
+            document.getElementById("todo-list").addEventListener("click", this.delete.bind(this));
+            document.getElementById("todo-list").addEventListener("animationend", this.remove);
+        }.bind(this));
+    },
+    add : function(e) {
+        if(e.keyCode === this.ENTER_KEYCODE && e.target.value !== "") {
+            var todoList = document.getElementById("todo-list");
+            var todo = e.target.value;
 
-        this.value = "";
+            var myAdd = {
+                method: 'POST',
+                headers: new Headers({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}),
+                mode: 'cors',
+                body: 'todo=' + todo
+            }
+
+            fetch(this.make_req(this.URL, myAdd)).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                todoList.insertAdjacentHTML("afterbegin", template([{
+                    id: data.insertId,
+                    todo: todo
+                }]));
+                e.target.value = "";
+            }).catch(function(err) {
+                // Error :(
+            });
+        }
+    },
+    complete : function(e) {
+        if(e.target.classList.contains("toggle")) {
+            var targetTODO = e.target.parentNode.parentNode;
+            var url = this.URL + "/" + targetTODO.dataset.id;
+
+            var myComp = {
+                method: 'PUT',
+                headers: new Headers({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}),
+                mode: 'cors',
+                body: 'completed=' + (targetTODO.classList.contains("completed") ? 0 : 1)
+            }
+
+            fetch(this.make_req(url, myComp)).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                targetTODO.classList.toggle("completed");
+            }).catch(function(err) {
+                // Error :(
+            });
+        }
+    },
+    delete : function(e) {
+        if(e.target.classList.contains("destroy")) {
+            var targetTODO = e.target.parentNode.parentNode;
+            var url = this.URL + "/" + targetTODO.dataset.id;
+
+            var myDel = {
+                method: 'DELETE',
+                headers: new Headers({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"}),
+                mode: 'cors'
+            }
+
+            fetch(this.make_req(url, myDel)).then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                targetTODO.classList = "delete";
+            }).catch(function(err) {
+                // Error :(
+            });
+        }
+    },
+    remove : function(e) {
+        if(e.animationName === "slideout" && e.target.classList.contains("delete")) {
+            e.currentTarget.removeChild(e.target);
+        }
+    },
+    make_req : function(url, options) {
+        return new Request(url, options);
     }
 }
 
-function completeTODO(e) {
-    if(e.target.classList.contains("toggle")) {
-        e.target.parentNode.parentNode.classList.toggle("completed");
-    }
-}
-
-function deleteTODO(e) {
-    if(e.target.classList.contains("destroy")) {
-        e.target.parentNode.parentNode.classList = "delete";
-        // e.currentTarget.removeChild(e.target.parentNode.parentNode);
-    }
-}
-
-function removeTODO(e) {
-    if(e.animationName === "slideout" && e.target.classList.contains("delete")) {
-        // e.target.parentNode.parentNode.classList = "delete";
-        
-        e.currentTarget.removeChild(e.target);
-    }
-}
-
-document.getElementById("new-todo").addEventListener("keydown", addTODO);
-document.getElementById("todo-list").addEventListener("click", completeTODO);
-document.getElementById("todo-list").addEventListener("click", deleteTODO);
-document.getElementById("todo-list").addEventListener("animationend", removeTODO);
+TODO.init();
